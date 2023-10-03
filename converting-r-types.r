37647c1e9d250ac6d6ba4d3de7f7eb5a80791317
Beregszászi István <beistvan@gmail.com>
2023-10-03 19:15:26 +0200

    Converting R Variables Of Some Data Types

---
+++ converting-r-types.r
@@ -0,0 +1,16 @@
+print(7 + 7) # This is R code
+
+x <- as.character(5)
+print(x)
+
+y <-as.numeric("7.5")
+print(y)
+
+z <- as.integer(8.9)
+print(z)
+
+a <- as.logical(0)
+print(a)
+
+b <- "TRUE"  
+print(as.logical(b))
