ae71755f8976bf5c98de4a9223561d7fe97f9cc9
Beregszászi István <beistvan@gmail.com>
2023-10-06 02:56:26 +0200

    Function Calls

---
+++ function-calls.r
@@ -0,0 +1,16 @@
+data <- c(120,22,22,31,15,120)
+unique_vals <- unique(data)
+print(unique_vals) # 120  22  31  15
+
+solution = sqrt(49)
+print(solution) # 7
+
+round_down = floor(3.14)
+round_up = ceiling(3.14)
+
+print(round_down) # 3
+print(round_up) # 4
+
+sort(c(2,4,10,5,1)); # 1  2  4  5 10
+length(c(2,4,10,5,1)); # 5
+sum(5,15,10) # 30
